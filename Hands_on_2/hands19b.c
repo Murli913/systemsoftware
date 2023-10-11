6b6115820f715f0a5b1907c6217d1c3cc4bc1504
Murli913 <141387276+Murli913@users.noreply.github.com>
2023-10-11 17:28:47 +0530

    Add files via upload

---
+++ Hands_on_2/hands19b.c
@@ -0,0 +1,25 @@
+/*
+============================================================================
+Name : hands19b.c
+Author : Murli Talreja
+Description : Create a FIFO file by
+b. mkfifo command
+Date: 28th Aug, 2023.
+============================================================================
+*/
+
+
+
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+
+int main()
+{
+	execlp("mkfifo","mkfifo","fifo1",NULL);
+
+	return 0;
+}
